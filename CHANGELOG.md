5e61a798f4ad240948b5799d0115dc760ab26461
Ben Allfree <ben@benallfree.com>
2024-09-12 22:47:32 -0700

    wip

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# pocketbase-stringify
+
+## 0.0.2
+
+### Patch Changes
+
+- Initial release
+- Adjusted replacer to use defaultReplacer if none is specified
+- Removed package links from deps and updated package info
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export const defaultReplacer = (k: string, v: any) => {
   if (v instanceof Error) {
-    return v.stack
+    return `${v}\n${v.stack}`
   }
   if (v instanceof RegExp) {
     return v.toString()
@@ -11,18 +11,19 @@ export const defaultReplacer = (k: string, v: any) => {
   return v
 }
 
-export const stringify = (obj: any, replacer = defaultReplacer, space = 0) => {
+export const stringify = (obj: any, _replacer = defaultReplacer, space = 0) => {
+  const replacer = _replacer || defaultReplacer
   const seen = new WeakSet()
   return JSON.stringify(
     obj,
     (k, v) => {
       if (typeof v === 'object' && v !== null) {
         if (seen.has(v)) {
-          return replacer ? replacer(k, `[Circular]`) : `[Circular]`
+          return replacer(k, `[Circular]`)
         }
         seen.add(v)
       }
-      return replacer ? replacer(k, v) : v
+      return replacer(k, v)
     },
     space
   )
